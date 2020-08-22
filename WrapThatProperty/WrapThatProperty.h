81f73626d9d771ccabcabe8fec3a0f4d85b432aa
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2020-08-22 11:56:57 +0530

    first commit

---
+++ WrapThatProperty/WrapThatProperty.h
@@ -0,0 +1,16 @@
+//
+//  WrapThatProperty.h
+//  WrapThatProperty
+//
+//  Created by Prakhar Tripathi on 22/08/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WrapThatProperty.
+FOUNDATION_EXPORT double WrapThatPropertyVersionNumber;
+
+//! Project version string for WrapThatProperty.
+FOUNDATION_EXPORT const unsigned char WrapThatPropertyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WrapThatProperty/PublicHeader.h>
